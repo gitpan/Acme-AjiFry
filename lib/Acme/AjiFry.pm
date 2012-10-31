10fba9802097ee5b05fd0ca079c70700ce036480
Taiki Kawakami <moznion@gmail.com>
2012-10-31 06:54:00 +0000

    Import of MOZNION/Acme-AjiFry-0.05 from CPAN.
    gitpan-cpan-distribution: Acme-AjiFry
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         MOZNION/Acme-AjiFry-0.05.tar.gz
    gitpan-cpan-author:       MOZNION
    gitpan-cpan-maturity:     released

--- lib/Acme/AjiFry.pm
+++ lib/Acme/AjiFry.pm
@@ -9,7 +9,7 @@ use Encode;
 use List::Util;
 use base 'Class::Accessor::Fast';
 
-use version; our $VERSION = '0.04';
+use version; our $VERSION = '0.05';
 
 our %cols;
 our %rows;
@@ -315,7 +315,7 @@ Acme::AjiFry - AjiFry Language (アジフライ語) Translator
 
 =head1 VERSION
 
-This document describes Acme::AjiFry version 0.0.4
+This document describes Acme::AjiFry version 0.0.5
 
 
 =head1 SYNOPSIS
@@ -325,7 +325,7 @@ This document describes Acme::AjiFry version 0.0.4
     my $ajifry = Acme::AjiFry->new();
 
     print $ajifry->translate_to_ajifry('おさしみ')."\n"; # outputs => "食えアジフライお刺身食え食えお刺身ドボドボ岡星ドボドボ"
-    print $ajifry->translate_from_ajifry('食えアジフライお刺身食え食えお刺身ドボドボ岡星ドボドボ')."\n"; # outputs => "食えアジフライお刺身食え食えお刺身ドボドボ岡星ドボドボ"
+    print $ajifry->translate_from_ajifry('食えアジフライお刺身食え食えお刺身ドボドボ岡星ドボドボ')."\n"; # outputs => "おさしみ"
 
 
 =head1 DESCRIPTION
